5dd6005f70e9a9ea22993ce7a6ef53cbccf728fe
Jessica Butts <butts029@umn.edu>
2020-02-12 07:57:06 -0600

    corrected spelling error in stimver; corrected analysis to give proportion instead of count and to give usable cases instead of unusable cases

--- R/week4.R
+++ R/week4.R
@@ -4,7 +4,7 @@ setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 
 # Data Import
 library(tidyverse)
-week4_df <- read_delim("../data/week4.dat", delim = "-", col_names = c("casenum", "parnum", "stimvar", "datadate", "qs"))
+week4_df <- read_delim("../data/week4.dat", delim = "-", col_names = c("casenum", "parnum", "stimver", "datadate", "qs"))
 glimpse(week4_df)
 week4_df <- separate(week4_df, qs, c("q1", "q2", "q3", "q4", "q5"), sep = " - ")
 week4_df[, c("q1", "q2", "q3", "q4", "q5")] <- sapply(week4_df[, c("q1", "q2", "q3", "q4", "q5")], as.numeric)
@@ -17,5 +17,5 @@ week4_df$datadate <- as.POSIXct(week4_df$datadate, format = "%b %e %Y, %H:%M:%S"
 
 
 # Analysis
-q2_over_time_df <- spread(week4_df[,c("parnum", "stimvar", "q2")], stimvar, q2)
-sum(apply(q2_over_time_df[,2:5], 1, function(x) {any(is.na(x))}))
+q2_over_time_df <- spread(week4_df[,c("parnum", "stimver", "q2")], stimver, q2)
+sum(apply(q2_over_time_df[,2:5], 1, function(x) {!any(is.na(x))}))/NROW(q2_over_time_df)
